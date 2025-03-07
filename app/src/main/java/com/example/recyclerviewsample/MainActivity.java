9b7cbba01cc946447eba363f1a26c691fd058784
Rohan Hasabe <rohanhasabe8@gmail.com>
2025-03-06 21:31:46 -0500

    Update the UI, and crash fix

--- app/src/main/java/com/example/recyclerviewsample/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewsample/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -18,12 +24,12 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     //Creating the essential parts needed for a Recycler view to work: RecyclerView, Adapter, LayoutManager
     private ArrayList<ItemCard> itemList = new ArrayList<>();
-    ;
 
     private RecyclerView recyclerView;
     private RviewAdapter rviewAdapter;
     private RecyclerView.LayoutManager rLayoutManger;
     private FloatingActionButton addButton;
+    private MaterialToolbar toolbar;
 
     private static final String KEY_OF_INSTANCE = "KEY_OF_INSTANCE";
     private static final String NUMBER_OF_ITEMS = "NUMBER_OF_ITEMS";
@@ -33,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup toolbar - make sure the view exists before setting it
+        toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
 
         init(savedInstanceState);
 
@@ -45,8 +57,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Specify what action a specific gesture performs, in this case swiping right or left deletes the entry
+        setupSwipeToDelete();
+    }
+
+    private void setupSwipeToDelete() {
+        // Add background color and icon for swipe-to-delete
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            private final ColorDrawable background = new ColorDrawable(Color.parseColor("#FF5252"));
+            
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
                 return false;
@@ -54,12 +72,44 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                Toast.makeText(MainActivity.this, "Delete an item", Toast.LENGTH_SHORT).show();
                 int position = viewHolder.getLayoutPosition();
+                String itemName = itemList.get(position).getItemName();
+                ItemCard deletedItem = itemList.get(position);
+                
                 itemList.remove(position);
-
                 rviewAdapter.notifyItemRemoved(position);
-
+                
+                // Show snackbar with undo option
+                Snackbar.make(recyclerView, "Deleted " + itemName, Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                itemList.add(position, deletedItem);
+                                rviewAdapter.notifyItemInserted(position);
+                                recyclerView.scrollToPosition(position);
+                            }
+                        }).show();
+            }
+            
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, 
+                                    @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, 
+                                    int actionState, boolean isCurrentlyActive) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                
+                View itemView = viewHolder.itemView;
+                
+                if (dX > 0) { // Swiping to the right
+                    background.setBounds(itemView.getLeft(), itemView.getTop(), 
+                            itemView.getLeft() + ((int) dX), itemView.getBottom());
+                } else if (dX < 0) { // Swiping to the left
+                    background.setBounds(itemView.getRight() + ((int) dX), itemView.getTop(),
+                            itemView.getRight(), itemView.getBottom());
+                } else { // No swipe
+                    background.setBounds(0, 0, 0, 0);
+                }
+                
+                background.draw(c);
             }
         });
         itemTouchHelper.attachToRecyclerView(recyclerView);
@@ -69,8 +119,6 @@ public class MainActivity extends AppCompatActivity {
     // Handling Orientation Changes on Android
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-
-
         int size = itemList == null ? 0 : itemList.size();
         outState.putInt(NUMBER_OF_ITEMS, size);
 
@@ -87,21 +135,17 @@ public class MainActivity extends AppCompatActivity {
             outState.putBoolean(KEY_OF_INSTANCE + i + "3", itemList.get(i).getStatus());
         }
         super.onSaveInstanceState(outState);
-
     }
 
     private void init(Bundle savedInstanceState) {
-
         initialItemData(savedInstanceState);
         createRecyclerView();
     }
 
     private void initialItemData(Bundle savedInstanceState) {
-
         // Not the first time to open this Activity
         if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
             if (itemList == null || itemList.size() == 0) {
-
                 int size = savedInstanceState.getInt(NUMBER_OF_ITEMS);
 
                 // Retrieve keys we stored in the instance
@@ -122,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        // The first time to opne this Activity
+        // The first time to open this Activity
         else {
             ItemCard item1 = new ItemCard(R.drawable.pic_gmail_01, "Gmail", "Example description", false);
             ItemCard item2 = new ItemCard(R.drawable.pic_google_01, "Google", "Example description", false);
@@ -131,12 +175,9 @@ public class MainActivity extends AppCompatActivity {
             itemList.add(item2);
             itemList.add(item3);
         }
-
     }
 
     private void createRecyclerView() {
-
-
         rLayoutManger = new LinearLayoutManager(this);
 
         recyclerView = findViewById(R.id.recycler_view);
@@ -148,7 +189,6 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(int position) {
                 //attributions bond to the item has been changed
                 itemList.get(position).onItemClick(position);
-
                 rviewAdapter.notifyItemChanged(position);
             }
 
@@ -156,22 +196,35 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckBoxClick(int position) {
                 //attributions bond to the item has been changed
                 itemList.get(position).onCheckBoxClick(position);
-
                 rviewAdapter.notifyItemChanged(position);
+                
+                // Show a snackbar when item is checked/unchecked
+                String status = itemList.get(position).getStatus() ? "checked" : "unchecked";
+                Snackbar.make(recyclerView, itemList.get(position).getItemName() + " " + status, 
+                        Snackbar.LENGTH_SHORT).show();
             }
         };
         rviewAdapter.setOnItemClickListener(itemClickListener);
 
         recyclerView.setAdapter(rviewAdapter);
         recyclerView.setLayoutManager(rLayoutManger);
-
-
     }
 
     private void addItem(int position) {
-        itemList.add(position, new ItemCard(R.drawable.empty, "No Logo item", "Item id: " + Math.abs(new Random().nextInt(100000)), false));
-        Toast.makeText(MainActivity.this, "Add an item", Toast.LENGTH_SHORT).show();
-
+        String[] descriptions = {
+            "A new item in your list",
+            "Check this out!",
+            "Important item to review",
+            "Added just now",
+            "Item id: " + Math.abs(new Random().nextInt(100000))
+        };
+        
+        int randomDescIndex = new Random().nextInt(descriptions.length);
+        itemList.add(position, new ItemCard(R.drawable.empty, "New Item", descriptions[randomDescIndex], false));
+        
         rviewAdapter.notifyItemInserted(position);
+        recyclerView.smoothScrollToPosition(position);
+        
+        Snackbar.make(recyclerView, "New item added", Snackbar.LENGTH_SHORT).show();
     }
 }
